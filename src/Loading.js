cb06255dffcaf8b247c14be08307157e76d84b0f
Mohamed Bouarada <bouarada7.mb@gmail.com>
2021-09-30 20:08:35 +0100

    fixing the loading page

--- src/Loading.js
+++ src/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
     return (
         <div className="loading">
-            <h1>Stanna 3asba</h1>
+            <h1>Loading ...</h1>
         </div>
     );
 };
